874fd9959ff3ed3d37aae08390672da384ad4dae
BrettRD <brett.downing1@gmail.com>
2015-11-29 14:09:47 +0800

    reduced code duplication and added basic serial output

--- DashBridge.ino
+++ DashBridge.ino
@@ -19,98 +19,63 @@
 //pull the constants out of ChallengerEV.h
 
 const uint32_t dashID1 = wheel[1].dashID;   //telemetry messages from the wheel manager
-const uint32_t dashID2= wheel[2].dashID;    //telemetry messages from the wheel manager
+const uint32_t dashID2 = wheel[2].dashID;    //telemetry messages from the wheel manager
 const uint32_t dashID3 = wheel[3].dashID;   //telemetry messages from the wheel manager
 const uint32_t dashID4 = wheel[4].dashID;   //telemetry messages from the wheel manager
 const uint32_t pedalID = pedalID;           //telemetry messages from the wheel manager
 
-// I D K
-  FlexCAN CANbus(1000000);
-  CANcallbacks canbus(&CANbus);
+FlexCAN CANbus(1000000);
+CANcallbacks canbus(&CANbus);
 
 
-  bool redLightBit = false;
-  bool greenLightBit = false;
-  // variables for wheel 1
-  uint16_t RPM1;
-  uint8_t voltage1;
-  uint8_t current1;
-  uint8_t throttle1;
-  uint8_t brake1;
-  uint8_t telemBits1;
-  // variables for wheel 2
-  uint16_t RPM2;
-  uint8_t voltage2;
-  uint8_t current2;
-  uint8_t throttle2;
-  uint8_t brake2;
-  uint8_t telemBits2;
-  // variables for wheel 3
-  uint16_t RPM3;
-  uint8_t voltage3;
-  uint8_t current3;
-  uint8_t throttle3;
-  uint8_t brake3;
-  uint8_t telemBits3;
-  //variables for wheel 4
-  uint16_t RPM4;
-  uint8_t voltage4;
-  uint8_t current4;
-  uint8_t throttle4;
-  uint8_t brake4;
-  uint8_t telemBits4;
 
-  //varaibles to store the average value
-  uint32_t aRPM;
-  uint16_t avoltage;
-  uint16_t acurrent;
-  uint16_t athrottle;
-  uint16_t abrake;
-  uint16_t atelemBits;
-  
-bool wheel1ProcessMessage (CAN_message_t &message){
-     RPM1 = ((uint16_t)message.buf[0])<<8;
-     RPM1 |= ((uint16_t)message.buf[1]);
-     voltage1 = message.buf[2];
-     current1 = message.buf[3];
-     throttle1 = message.buf[4];
-     brake1 = message.buf[5];
-     telemBits1 =message.buf[6];
+
+struct WheelStats{
+  uint16_t RPM;
+  uint8_t voltage;
+  uint8_t current;
+  uint8_t throttle;
+  uint8_t brake;
+  uint8_t telemBits;
+  bool redLight;
+  bool greenLight;
+};  
+
+WheelStats wheelstats[nWheels] = {0};
+
+//varaibles to store the average value
+uint32_t avgRPM;
+uint16_t avgVoltage;
+uint16_t avgCurrent;
+uint16_t avgThrottle;
+uint16_t avgBrake;
+
+bool wheelnProcessMessage(int n, CAN_message_t &message){
+  wheelstats[n].RPM = ((uint16_t)message.buf[0])<<8;
+  wheelstats[n].RPM |= ((uint16_t)message.buf[1]);
+  wheelstats[n].voltage = message.buf[2];
+  wheelstats[n].current = message.buf[3];
+  wheelstats[n].throttle = message.buf[4];
+  wheelstats[n].brake = message.buf[5];
+  wheelstats[n].telemBits =message.buf[6];
+  return true;
 }
 
+bool wheel1ProcessMessage (CAN_message_t &message){
+  return wheelnProcessMessage(1, message);
+}
 bool wheel2ProcessMessage (CAN_message_t &message){
-     RPM2 = ((uint16_t)message.buf[0])<<8;
-     RPM2 |= ((uint16_t)message.buf[1]);
-     voltage2 = message.buf[2];
-     current2 = message.buf[3];
-     throttle2 = message.buf[4];
-     brake2 = message.buf[5];
-     telemBits2 = message.buf[6];
-  
+  return wheelnProcessMessage(2, message);
 }
 bool wheel3ProcessMessage (CAN_message_t &message){
-     RPM3 = ((uint16_t)message.buf[0])<<8;
-     RPM3 |= ((uint16_t)message.buf[1]);
-     voltage3 = message.buf[2];
-     current3 = message.buf[3];
-     throttle3 = message.buf[4];
-     brake3 = message.buf[5];
-     telemBits3 = message.buf[6];
-  
+  return wheelnProcessMessage(3, message);
 }
 bool wheel4ProcessMessage (CAN_message_t &message){
-     RPM4 = ((uint16_t)message.buf[0])<<8;
-     RPM4 |= ((uint16_t)message.buf[1]);
-     voltage4 = message.buf[2];
-     current4 = message.buf[3];
-     throttle4 = message.buf[4];
-     brake4 = message.buf[5];
-     telemBits4 = message.buf[6];
-  
+  return wheelnProcessMessage(4, message);
 }
 bool pedalProcessMessage (CAN_message_t &message){
-
-  
+  //placeholder
+  return true;
 }
 
 
@@ -118,8 +83,6 @@ bool pedalProcessMessage (CAN_message_t &message){
 
 
 void setup() {
-  // 
-
   Serial.begin(9600);
   Serial.println("Dash Display");
 
@@ -173,20 +136,43 @@ void setup() {
 void loop() {
   // check for new message
   CAN_message_t message;
-
+  //let the can callbacks handle unpacking the messages
   while(canbus.receive(message)) {
-
   }
 
-  aRPM =      (RPM1+RPM2+RPM3+RPM4)/4;
-  avoltage =  (voltage1+voltage2+voltage3+voltage4)/4;
-  acurrent =  (current1+current2+current3+current4)/4;
-  athrottle = (throttle1+throttle2+throttle3+throttle4)/4;
-  abrake =    (brake1+brake2+brake3+brake4)/4;
-  atelemBits =(telemBits1+telemBits2+telemBits3+telemBits4)/4;
-   
+  avgRPM = 0;
+  avgVoltage = 0;
+  avgCurrent = 0;
+  avgThrottle = 0;
+  avgBrake = 0;
 
-       
+  for(int i=0; i<nWheels; i++){
+    avgRPM      += wheelstats[i].RPM;
+    avgVoltage  += wheelstats[i].voltage;
+    avgCurrent  += wheelstats[i].current;
+    avgThrottle += wheelstats[i].throttle;
+    avgBrake    += wheelstats[i].brake;
+  }
+
+  avgRPM /= 4;
+  avgVoltage /= 4;
+  avgCurrent /= 4;
+  avgThrottle /= 4;
+  avgBrake /= 4;
+
+  Serial.print(avgRPM);
+  Serial.print(",");
+  Serial.print(avgVoltage);
+  Serial.print(",");
+  Serial.print(avgCurrent);
+  Serial.print(",");
+  Serial.print(avgThrottle);
+  Serial.print(",");
+  Serial.print(avgBrake);
+
+  //Serial.print(",");
+  Serial.println("");
+
 }
 
     
